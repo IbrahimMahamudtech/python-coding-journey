37754e2e56e270e103c5b155065178c5c22768a1
Ibrahim Mahamud <aabdi5341@gmail.com>
2024-10-14 21:36:43 +0100

    initial commit

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band generator")
+City = input("What city are you born in?\n")
+Pet = input("What is the name of your first pet?\n")
+print("Your pet name could be " + City + " " + Pet)
